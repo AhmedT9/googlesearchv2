ff5a1b8c26fd8c6a0bc37996fa014b0167618285
AhmedT9 <toumi_a9@hotmail.com>
2024-02-10 15:58:09 +0000

    Initial commit

--- lang/en/block_googlesearch.php
+++
@@ -1,4 +0,0 @@
-<?php
-$string['pluginname'] = 'Google Search Block';
-$string['googlesearch:addinstance'] = 'Add a new Google Search block';
-$string['googlesearch:myaddinstance'] = 'Add a new Google Search block to the My Moodle page';
---
+++ lang/en/block_googlesearchv2.php
@@ -0,0 +1,4 @@
+<?php
+$string['pluginname'] = 'Google Search Block v2';
+$string['googlesearchv2:addinstance'] = 'Add a new Google Search block';
+$string['googlesearchv2:myaddinstance'] = 'Add a new Google Search block to the My Moodle page';
